646e731a6a763d61114eb4af7a39ab543a674dcc
ECCC-CSSP <Charles.Leblanc@ec.gc.ca>
2017-10-11 11:40:06 -0300

    Init

--- CSSPWebTools/Controllers/SamplingPlanController.cs
+++ CSSPWebTools/Controllers/SamplingPlanController.cs
@@ -451,9 +451,9 @@ namespace CSSPWebTools.Controllers
         #region Functions JSON
         [HttpPost]
         [OutputCache(Location = OutputCacheLocation.None, NoStore = true)]
-        public JsonResult LabSheetAcceptedJSON(int LabSheetID, int TimeOffsetMinutes, int AnalyzeMethod, int SampleMatrix, int Laboratory) //, string ChangeRunSamplingType)
+        public JsonResult LabSheetAcceptedJSON(int LabSheetID, int TimeOffsetMinutes, int AnalyzeMethod, int SampleMatrix, int Laboratory, string ChangeRunSamplingType)
         {
-            LabSheetModel labSheetModel = _LabSheetService.LabSheetAcceptedDB(LabSheetID, TimeOffsetMinutes, AnalyzeMethod, SampleMatrix, Laboratory); //, ChangeRunSamplingType);
+            LabSheetModel labSheetModel = _LabSheetService.LabSheetAcceptedDB(LabSheetID, TimeOffsetMinutes, AnalyzeMethod, SampleMatrix, Laboratory, ChangeRunSamplingType);
 
             return Json(labSheetModel.Error, JsonRequestBehavior.AllowGet);
         }
