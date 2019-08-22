ad0f2d743ec25716978f6b0b75be9aed5f66d1ed
ECCC-CSSP <charles.leblanc2@canada.ca>
2019-08-22 16:15:26 -0300

    Init

--- CSSPWebTools/Views/Document/Resources/DocumentViewRes.Designer.cs
+++ CSSPWebTools/Views/Document/Resources/DocumentViewRes.Designer.cs
@@ -19,7 +19,7 @@ namespace CSSPWebTools.Views.Document.Resources {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class DocumentViewRes {
@@ -111,6 +111,15 @@ namespace CSSPWebTools.Views.Document.Resources {
         public static string Download {
             get {
                 return ResourceManager.GetString("Download", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Estimated Drogue Paths Animation.
+        /// </summary>
+        public static string EstimatedDroguePathsAnimation {
+            get {
+                return ResourceManager.GetString("EstimatedDroguePathsAnimation", resourceCulture);
             }
         }
         
